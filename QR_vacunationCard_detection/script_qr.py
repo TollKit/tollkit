4d6d2cbd63156b857fb152687a932b177582d1b0
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-08-27 16:18:47 -0500

    script QR

--- QR_vacunationCard_detection/script_qr.py
+++ QR_vacunationCard_detection/script_qr.py
@@ -1,1 +1,38 @@
 # ---
+
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+#img=cv2.imread('descarga.png')
+
+cap=cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+import time
+print('Empezando')
+while True:
+    success, img=cap.read()
+    try:
+        for barcode in decode(img):
+            print(barcode.data)
+            myData=barcode.data.decode('utf-8')
+            if 'minsa' in myData:
+                print(type(myData))
+                pts=np.array([barcode.polygonl.np.int32])
+                pts=pts.reshape((-1,1,2)) 
+                cv2.polylines(img,[pts],True,(255,0,255),5)
+            else: print('Por favor presente un QR válido')
+        cv2.imshow('Result',img)
+        key= cv2.waitKey(1)
+        if key==27:
+            break
+        #time.sleep(5)
+    except:
+        print('-'*100)
+        print('Se ha detectado su carnet de vacunación')
+#code=decode(img)
+#print(code)
+for barcode in decode(img):
+    print(barcode.data)
+    myData=barcode.data.decode('utf-8')
+    print(myData)
