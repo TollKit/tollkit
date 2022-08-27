cbed48cbdfca362b4cc1517fa17278c52775262b
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-08-27 16:17:45 -0500

    fix2

--- tollkit/server.py
+++
@@ -1,8 +0,0 @@
-from flask_app import app
-
-#Importación de controladores
-from flask_app.controllers import admins_controller, tollkits_controller
-
-if __name__=="__main__":
-    app.run(debug=True)
-
